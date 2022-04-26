ce830f3bf9a8acc3e13b084232f2ae235493de02
Mister-Teapot <93923285+Mister-Teapot@users.noreply.github.com>
2022-04-26 11:45:03 +0200

    minor changes

--- ncbi_download_small.py
+++ ncbi_download_small.py
@@ -6,4 +6,4 @@ import ncbi_genome_download as ngd
 
 ngd.download(section='refseq', file_formats='fasta', refseq_categories='reference', assembly_levels='complete', groups='bacteria')
 # refseq_categories='reference' downloads 15 sequences (24-04-2022)
-# refseq_categories='representative' downloads >333 sequences (24-04-2022) (I didn't finish the download)
+# refseq_categories='representative' downloads ~15k sequences (24-04-2022) (I didn't finish the download)
