17036e65989fda7e409431ae0c0dd003ec5c8b4d
Mister-Teapot <93923285+Mister-Teapot@users.noreply.github.com>
2022-05-03 20:01:10 +0200

    Merge DoriC with my prediction in one csv

--- oriC_comparison.py
+++ oriC_comparison.py
@@ -10,27 +10,41 @@ from Bio import pairwise2 # Apperently it's better to use Align, but I can't get
 
 # Pandas printing options
 pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', 25)
+pd.set_option('display.max_colwidth', 20)
 
 
 # Read CSVs
 Z_curve_df = pd.read_csv( 'NCBI data prep/NCBI_oriC_15_improved.csv' )
-DoriC_df   = pd.read_csv( 'DoriC data prep/DoriC_oriC_seperate_entries.csv' )
+DoriC_df   = pd.read_csv( 'DoriC data prep/DoriC_oriC_concat_entries.csv' )
 
 # Setting columns to proper types
 Z_curve_df = Z_curve_df.astype({'Sequence_length':'float', 'False_order':'bool'})
 Z_curve_df['Penalties'] = Z_curve_df['Penalties'].map(literal_eval)
 
-oriC_cols = [i for i in Z_curve_df.columns if 'oriC' in i]
-most_oriC = int(sorted(oriC_cols, key=lambda x:x[-1], reverse=True)[0][-1]) + 1
-for i in range(most_oriC):
+oriC_cols_Z = [i for i in Z_curve_df.columns if 'oriC' in i]
+most_oriC_Z = int(sorted(oriC_cols_Z, key=lambda x:x[-1], reverse=True)[0][-1]) + 1
+for i in range(most_oriC_Z):
     Z_curve_df = Z_curve_df.astype({f'oriC_middles_{i}':'float'})
-
     Z_curve_df[f'oriC_edges_{i}'].fillna('()', inplace=True)
     Z_curve_df[f'oriC_edges_{i}'] = Z_curve_df[f'oriC_edges_{i}'].map(literal_eval)
 
-# All DoriC columns have been interpreted properly
-# print(DoriC_df.dtypes)
+oriC_cols_D = [i for i in DoriC_df.columns if 'oriC' in i]
+most_oriC_D = int(sorted(oriC_cols_D, key=lambda x:x[-1], reverse=True)[0][-1]) + 1
+for i in range(most_oriC_D):
+    DoriC_df[f'DoriC_oriC_{i}'].fillna('()', inplace=True)
+    DoriC_df[f'DoriC_oriC_{i}'] = DoriC_df[f'DoriC_oriC_{i}'].map(literal_eval)
+
+col_names_Z = [x for x in Z_curve_df.columns if x not in ['RefSeq', 'Organism']]
+in_both_df = {x: [] for x in DoriC_df.columns.to_list() + col_names_Z}
+for i, sample in Z_curve_df.iterrows():
+    if sample['RefSeq'] in DoriC_df['RefSeq'].to_list():
+        for j in col_names_Z:
+            in_both_df[j].append( sample[j] )
+        for j in DoriC_df.columns.to_list():
+            in_both_df[j].append( DoriC_df.loc[DoriC_df['RefSeq'] == sample['RefSeq'], j].values[0] )
+pd.DataFrame(in_both_df).to_csv('test.csv')
+
+
 
 # checked = 0
 # partial_oriC = []
