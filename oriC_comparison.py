b190aa520754ebe325cfd9c6e17545eca4044b06
Mister-Teapot <93923285+Mister-Teapot@users.noreply.github.com>
2022-05-04 15:58:40 +0200

    Update oriC_comparison.py

--- oriC_comparison.py
+++ oriC_comparison.py
@@ -1,5 +1,6 @@
 import os
 from ast import literal_eval
+from itertools import product
 import pandas as pd
 
 # Set cwd to location of this script
@@ -9,30 +10,12 @@ os.chdir( os.path.dirname( os.path.abspath(__file__) ) )
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', 15)
 
-def compare_dbs(Z_curve_csv, DoriC_csv, comparator_csv=None):
+def make_comparator_csv(Z_curve_csv, DoriC_csv, comparator_csv):
+    '''Merges Z_curve_csv and DoriC_csv into one csv and/or dataframe for comparison with compare_dbs'''
 
     # Read CSVs
     Z_curve_df = pd.read_csv( Z_curve_csv )
     DoriC_df   = pd.read_csv( DoriC_csv )
-
-    # Completely useless! Dummy...
-    # Setting columns to proper types: Z-curve
-    # Z_curve_df = Z_curve_df.astype({'Sequence_length':'float', 'False_order':'bool'})
-    # Z_curve_df['Penalties'] = Z_curve_df['Penalties'].map(literal_eval)
-
-    # oriC_cols_Z = [i for i in Z_curve_df.columns if 'oriC' in i]
-    # most_oriC_Z = int(sorted(oriC_cols_Z, key=lambda x:x[-1], reverse=True)[0][-1]) + 1
-    # for i in range(most_oriC_Z):
-    #     Z_curve_df = Z_curve_df.astype({f'oriC_middles_{i}':'float'})
-    #     Z_curve_df[f'oriC_edges_{i}'].fillna('()', inplace=True)
-    #     Z_curve_df[f'oriC_edges_{i}'] = Z_curve_df[f'oriC_edges_{i}'].map(literal_eval)
-
-    # # Setting columns to proper types: DoriC
-    # oriC_cols_D = [i for i in DoriC_df.columns if 'oriC' in i]
-    # most_oriC_D = int(sorted(oriC_cols_D, key=lambda x:x[-1], reverse=True)[0][-1]) + 1
-    # for i in range(most_oriC_D):
-    #     DoriC_df[f'DoriC_oriC_{i}'].fillna('()', inplace=True)
-    #     DoriC_df[f'DoriC_oriC_{i}'] = DoriC_df[f'DoriC_oriC_{i}'].map(literal_eval)
 
     # Merge into single dataframe
     col_names_Z = [x for x in Z_curve_df.columns if x not in ['RefSeq', 'Organism']]
@@ -44,32 +27,43 @@ def compare_dbs(Z_curve_csv, DoriC_csv, comparator_csv=None):
             for j in DoriC_df.columns.to_list():
                 in_both_df[j].append( DoriC_df.loc[DoriC_df['RefSeq'] == sample['RefSeq'], j].values[0] ) # Series.values -> ['element'], very annoying
 
-    del Z_curve_df
-    del DoriC_df
-
     comparator_df = pd.DataFrame(in_both_df).head()
-    if comparator_csv is not None:
-        comparator_df.to_csv(comparator_csv, index=False)
+    comparator_df.to_csv(comparator_csv, index=False)
 
+    return comparator_df
+
+def compare_dbs(df=None, csv=None):
+    '''Compares results. Results can be loaded from a pandas.DataFrame or csv.'''
+
+    if df is None and csv is None:
+        raise KeyError('Must load a pandas.DataFrame or CSV file.')
+    if df is not None and csv is not None:
+        raise KeyError('Load either a pandas.DataFrame or CSV, not both.')
+
+    if csv is not None:
+        df = pd.read_csv(csv)
+
+    # Z_curve_df['Penalties'] = Z_curve_df['Penalties'].map(literal_eval) # For converting tuple-strings to tuple objects
     # Compare predictions (Assuming DoriC as ground truth for now)
     num_of_predictions = [0, 0] # [more oriC in DoriC, more oriC by mine]
-    for i, sample_original in comparator_df.iterrows():
+    for i, sample_original in df.iterrows():
         sample = sample_original.copy()
         sample.dropna(inplace=True)
 
-        num_oriC_D = int( sorted([i for i in sample.axes[0] if 'DoriC_oriC' in i], key=lambda x:x[-1], reverse=True)[0][-1] ) + 1
-        num_oriC_Z = int( sorted([i for i in sample.axes[0] if 'oriC_edges' in i], key=lambda x:x[-1], reverse=True)[0][-1] ) + 1
+        D_oriC_cols = sorted( [i for i in sample.axes[0] if 'DoriC_oriC' in i], key=lambda x:x[-1] )
+        Z_oriC_cols = sorted( [i for i in sample.axes[0] if 'oriC_edges' in i], key=lambda x:x[-1] )
 
-        if num_oriC_D > num_oriC_Z:
+        if len(D_oriC_cols) > len(Z_oriC_cols):
             num_of_predictions[0] += 1
-            ...
-        elif num_oriC_D < num_oriC_Z:
+        elif len(D_oriC_cols) < len(Z_oriC_cols):
             num_of_predictions[1] += 1
-            ...
-        else: # Equal amount of oriC predictions
+        for oriC_D, oriC_Z in product(D_oriC_cols, Z_oriC_cols):
             ...
 
 if __name__ == '__main__':
-    Z_curve_csv = 'NCBI data prep/refseq_15/NCBI_oriC_15_improved.csv'
-    DoriC_csv   = 'DoriC data prep/DoriC_oriC_concat_entries.csv'
-    compare_dbs(Z_curve_csv, DoriC_csv, comparator_csv='Both_predictions.csv')
+    Z_curve_csv    = 'NCBI data prep/refseq_15/NCBI_oriC_15_improved.csv'
+    DoriC_csv      = 'DoriC data prep/DoriC_oriC_concat_entries.csv'
+    comparator_csv = 'Both_predictions.csv'
+
+    comparator_df = make_comparator_csv(Z_curve_csv, DoriC_csv, comparator_csv=comparator_csv)
+    compare_dbs(df=comparator_df) # csv=comparator_csv
