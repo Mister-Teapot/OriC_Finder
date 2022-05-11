387fd65ea8eb8d9d6adf5aa56c4fafcfcc8a0cb4
Mister-Teapot <93923285+Mister-Teapot@users.noreply.github.com>
2022-05-11 18:18:15 +0200

    made helper functions

---
+++ NCBI data prep/data_prep_helpers.py
@@ -0,0 +1,76 @@
+import csv
+import os
+import shutil
+
+os.chdir( os.path.dirname( os.path.abspath(__file__) ) )
+
+def merge_csvs(file_folder, merged_csv, fieldnames, length=-1, headers=False):
+    '''
+    Merge multiple csvs into one csv.
+    Arguments:
+        file_folder : path to folder with csvs that have to be merged
+        merged_csv  : name of the merged csv
+        fieldnames  : list of fieldnames for the merged_csv
+        length      : amount of rows in each single csv. -1, if the length of each single
+                      csv is not known or not the same.
+        headers     : if the single csvs have headers or not
+    '''
+    file_list = os.listdir(file_folder)
+    with open(merged_csv, 'w', newline='') as fh_out:
+        writer = csv.writer(fh_out)
+        writer.writerow(fieldnames)
+        for file in file_list:
+            with open(file_folder + file, 'r') as fh_in:
+                reader = csv.reader(fh_in)
+                for i, row in enumerate(reader):
+                    if headers and i == 0:
+                        pass
+                    elif i == length:
+                        break
+                    else:
+                        writer.writerow(row)
+    
+# df_3k = pd.read_csv(r'C:\0. School\Bachelor Thesis\Zoya_Code+Data\OriFinder\OriC_Finder\NCBI data prep\refseq_8\NCBI_oriC_7.csv')
+# df_rest = pd.read_csv(r'C:\0. School\Bachelor Thesis\Zoya_Code+Data\OriFinder\OriC_Finder\NCBI data prep\3k+299+15_merged.csv')
+# df_3k.dropna(how='all', axis=1, inplace=True)
+# df_rest.dropna(how='all', axis=1, inplace=True)
+# df_3k['RefSeq'] = df_3k['RefSeq'].str.extract(r'([^.]*)')
+
+# df_merged = pd.concat((df_3k, df_rest), axis=0, ignore_index=True)
+
+# df_merged.to_csv(r'C:\0. School\Bachelor Thesis\Zoya_Code+Data\OriFinder\OriC_Finder\NCBI data prep\3k+299+15+7_merged.csv', index=False)
+
+def move_fastas(db_loc, on_cluster=True, split=4):
+    '''
+    Split one folder into 'split' amount of folders with roughly the same amount of files in them.
+    Used for easier parallel processing. Instead of one big job with 4000 samples. Run 4 jobs with 1000 samples at the same time.
+    '''
+    if on_cluster:
+        path = db_loc + '/bacteria'
+        samples = os.listdir(path)
+    else:
+        path = db_loc + '/chromosomes_only'
+        samples = os.listdir(path)
+
+    samples_per_split = len(samples)//split
+    for i in range(split):
+        new_folder = db_loc + '/' + str(i)
+        os.mkdir(new_folder)
+        start = 0 + i*samples_per_split
+        stop  = (i+1)*samples_per_split if i != split-1 else None
+        for sample in samples[start:stop]:
+            shutil.move(path + '/' + sample, new_folder + '/' + sample)
+
+
+if __name__ == '__main__':
+
+    max_oriCs = 10
+    fieldnames = ['RefSeq', 'Organism', 'Sequence_length', 'N_penalty', 'O_penalty', 'D_penalty', 'False_order']
+    fieldnames.extend([f'oriC_edges_{i}' for i in range(max_oriCs)])
+    fieldnames.extend([f'oriC_middles_{i}' for i in range(max_oriCs)])
+    fieldnames.extend([f'xy_oriC_{i}' for i in range(max_oriCs)])
+    fieldnames.extend([f'xgc_oriC_{i}' for i in range(max_oriCs)])
+    fieldnames.extend([f'ygc_oriC_{i}' for i in range(max_oriCs)])
+
+    # merge_csvs('refseq_23/csvs/', 'refseq_23/merged.csv', fieldnames, length=1)
+    move_fastas('refseq_23', on_cluster=False, split=4)
---
+++ NCBI data prep/predict_dataset.py
@@ -0,0 +1,97 @@
+# Standard imports
+import multiprocessing as mp
+from functools import partial
+import csv
+import os, sys
+import numpy as np
+
+# Set these before running
+DATASET_NAME = 'refseq_23'
+ON_CLUSTER   = False
+PARALLEL     = False
+MAX_ORICS    = 10 # Assumption: No more than 10 oriC for a single organism are predicted
+
+# Self-made module
+if ON_CLUSTER:
+    # Cluster path
+    sys.path.append('/tudelft.net/staff-umbrella/GeneLocations/ZoyavanMeel/OriC_Finder/')
+    run_type = 'cluster'
+else:
+    # Local path
+    sys.path.append('../OriC_Finder')
+    run_type = 'local'
+from oriC_Finder import find_oriCs
+
+
+def init_settings(run_type, dataset_name, parallel):
+    '''Gets variables for processing based on whether the script is executed locally or on the cluster'''
+    type_list = ['cluster', 'local']
+    data_path, csv_path, cpus = None, None, None
+
+    if run_type not in type_list:
+        raise KeyError('Not a valid location')
+
+    if run_type == 'cluster':
+        data_path = '/tudelft.net/staff-umbrella/GeneLocations/ZoyavanMeel/' + dataset_name + '/bacteria'
+        csv_path  = '/tudelft.net/staff-umbrella/GeneLocations/ZoyavanMeel/' + dataset_name + '/csvs'
+        cpus      = len(os.sched_getaffinity(0)) if parallel else 1
+
+    if run_type == 'local':
+        data_path = 'NCBI data prep/' + dataset_name + '/chromosomes_only'
+        csv_path  = 'NCBI data prep/' + dataset_name + '/csvs'
+        cpus      = os.cpu_count() - 1 if parallel else 1
+
+    return data_path, csv_path, cpus
+
+
+def prep_prediction(sample_path, csv_path, max_oriCs):
+    preferred_properties, all_oriCs = find_oriCs(sample_path)
+
+    # Name processing
+    name_list = preferred_properties['name'].split(' ')
+    RefSeq = name_list[0]
+    Organism = ' '.join(name_list[1:])
+    row = []
+    row.append(RefSeq) # RefSeq = RefSeq Accession Number
+    row.append(Organism)
+    row.append(preferred_properties['seq_size'])
+
+    # Quality of Prediction processing
+    row.append(preferred_properties['n_penalty'])
+    row.append(preferred_properties['o_penalty'])
+    row.append(preferred_properties['d_penalty'])
+    row.append(preferred_properties['false_order'])
+
+    # OriC processing
+    for i in range(max_oriCs):
+        if i < len(preferred_properties['oriC_middles']):
+            row.append(preferred_properties['oriC_edges'][i])
+            row.append(preferred_properties['oriC_middles'][i])
+        else:
+            row.append(np.nan)
+            row.append(np.nan)
+
+    for i in range(max_oriCs):
+        row.append(all_oriCs['xy_oriCs'][i]) if i < len(all_oriCs['xy_oriCs']) else row.append(np.nan)
+    for i in range(max_oriCs):
+        row.append(all_oriCs['xgc_oriCs'][i]) if i < len(all_oriCs['xgc_oriCs']) else row.append(np.nan)
+    for i in range(max_oriCs):
+        row.append(all_oriCs['ygc_oriCs'][i]) if i < len(all_oriCs['ygc_oriCs']) else row.append(np.nan)
+
+    with open(csv_path + '/' + RefSeq + '.csv', 'w') as fh:
+        writer = csv.writer(fh)
+        writer.writerow(row)
+        fh.close()
+
+if __name__ == '__main__':
+    data_path, csv_path, cpus = init_settings(run_type, DATASET_NAME, PARALLEL)
+    samples = os.listdir( data_path )
+    sample_paths = [data_path + '/' + fasta for fasta in samples]
+    del samples
+    if not PARALLEL:
+        for sample_path in sample_paths:
+            prep_prediction(sample_path, csv_path, MAX_ORICS)
+    else:    
+        with mp.Pool(cpus) as pool:
+            prepped_prediction = partial(prep_prediction, csv_path=csv_path, max_oriCs=MAX_ORICS)
+            pool.map(prepped_prediction, sample_paths)
