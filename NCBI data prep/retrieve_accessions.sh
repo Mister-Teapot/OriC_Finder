77ee783a4cc668a9db537d8268b6ad337e0ab156
Zoya van Meel <hvanmeel@login1.hpc.tudelft.nl>
2022-06-14 13:30:09 +0200

    right file this time

--- NCBI data prep/retrieve_accessions.sh
+++ NCBI data prep/retrieve_accessions.sh
@@ -1,9 +1,14 @@
 BIOSAMPLES=$(esearch -db nuccore -query "rep_origin [FEATURE] AND bacteria[Organism]" | elink -target biosample | efetch -format docsum | xtract.Linux -pattern DocumentSummary -block Accession -element Accession)
-MDATA="mdata.tab"
-#echo -e "BioSample\tAssembly" >> ${MDATA} #Put a header in the file
+MDATA="mdata2.tab"
+echo -e "BioSample\tAccession\tRefSeq" >> ${MDATA} #Put a header in the file
 for BIOSAMPLE in ${BIOSAMPLES[@]}
 do
-    DOCSUM=$(esearch -db assembly -query ${BIOSAMPLE} | efetch -format docsum)
-    ASSEMBLY=$(echo ${DOCSUM} | xtract.Linux -pattern DocumentSummary -block Synonym -element RefSeq)
-    echo -e ${BIOSAMPLE}'\t'${ASSEMBLY} >> ${MDATA}
+    QUERY=$(esearch -db assembly -query ${BIOSAMPLE} | efetch -format docsum)
+    ACCESSION=$(echo ${QUERY} | xtract.Linux -pattern DocumentSummary -block Synonym -element RefSeq)
+    REFSEQ="True"
+    if [[ -z "$ACCESSION" ]]; then
+        ACCESSION=$(echo ${QUERY} | xtract.Linux -pattern DocumentSummary -block Synonym -element Genbank)
+        REFSEQ="False"
+    fi
+    echo -e ${BIOSAMPLE}'\t'${ACCESSION}'\t'${REFSEQ} >> ${MDATA}
 done
