182ec89432f4314c95fac178de9e4ea9723b75b7
Mister-Teapot <93923285+Mister-Teapot@users.noreply.github.com>
2022-06-22 11:24:16 +0200

    rep_origin in NCBI analysis

--- NCBI data prep/process_mdata.py
+++ NCBI data prep/process_mdata.py
@@ -1,19 +1,32 @@
 import pandas as pd
+from Bio import Entrez
+import multiprocessing as mp
 pd.set_option('display.width', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 
 table = pd.read_table('NCBI data prep/mdata.tab')
-
+refseqs = []
 for i, sample in table.iterrows():
-    if isinstance(sample['Accession'], float):
-        acc = ''
-    else:
-        acc = sample['Accession'].split(' ')[0]
-    table.at[i, 'Accession'] = acc
-table['Accession'] = table['Accession'].str.extract(r'([^.]*)')
-table.to_csv('NCBI data prep/mdata_clean.csv', index=False)
+    if isinstance(sample['ChromosomeAccession'], str):
+        accs = [ acc.split('.')[0] for acc in sample['ChromosomeAccession'].split(' ') if '_' in acc]
+        refseqs.extend(accs)
 
-refseqs = table[table['RefSeq'] == True]['Accession']
-for i in refseqs:
-    print(i)
+Entrez.email = 'zoyavanmeel@gmail.com'
+Entrez.api_key = '795d705fb638507c9b2295c89cc64ee88108'
+
+test = pd.read_table('NCBI data prep/test.tab')
+a = set(table['Assembly'].to_list()).intersection(test)
+print(len(a), a)
+
+# def do(i):
+#     print(f'Processing:\t{refseqs[i]}\t{i+1}/{len(refseqs)}')
+#     handle = Entrez.efetch(db='nuccore', id=refseqs[i], rettype='gb_with_parts', retmode='text')
+#     for j in handle:
+#         if 'rep_' in j or 'REP_' in j:
+#             print('Found rep_origin in:', refseqs[i])
+
+# if __name__ == '__main__':
+#     with mp.Pool(mp.cpu_count() - 1) as pool:
+#         pool.map(do, [x for x in range(len(refseqs))])
+# # print(len(refseqs))
--- NCBI data prep/retrieve_accessions.sh
+++ NCBI data prep/retrieve_accessions.sh
@@ -1,16 +1,30 @@
 # Make sure to have EntrezDirect: https://www.ncbi.nlm.nih.gov/books/NBK179288/
-# This script is based in large part on the README from: https://github.com/schultzm/entrez_direct_tut
-BIOSAMPLES=$(esearch -db nuccore -query "rep_origin [FEATURE] AND bacteria[Organism]" | elink -target biosample | efetch -format docsum | xtract.Linux -pattern DocumentSummary -block Accession -element Accession)
-MDATA="mdata2.tab"
-echo -e "BioSample\tAccession\tRefSeq" >> ${MDATA} #Put a header in the file
-for BIOSAMPLE in ${BIOSAMPLES[@]}
+# This script is based in part on the README from: https://github.com/schultzm/entrez_direct_tut
+# Some entries will return no chromosomes, but still show as fromRefSeq=true. AFAIK, these entries are either shotgun sequences or sequences that have been replaced by different entries.
+BIOSAMPLES=$(esearch -db nuccore -query "rep_origin [FEATURE] AND bacteria[Organism]" | elink -target assembly | efetch -format docsum | xtract.Linux -pattern DocumentSummary -block Synonym -element RefSeq)
+MDATA="mdata.tab"
+echo -e "Assembly\tChromosomeAccession\tfromRefSeq" >> ${MDATA}
+for BIOSAMPLE in ${BIOSAMPLES}
 do
-    QUERY=$(esearch -db assembly -query ${BIOSAMPLE} | efetch -format docsum)
-    ACCESSION=$(echo ${QUERY} | xtract.Linux -pattern DocumentSummary -block Synonym -element RefSeq)
-    REFSEQ="True"
-    if [[ -z "$ACCESSION" ]]; then
-        ACCESSION=$(echo ${QUERY} | xtract.Linux -pattern DocumentSummary -block Synonym -element Genbank)
+    RESULT=$(esearch -db nuccore -query ${BIOSAMPLE} | efetch -format docsum | xtract.Linux -pattern DocumentSummary -element Genome,AccessionVersion)
+    declare -a CHROMOSOMES=()
+    if [[ -z "$RESULT" ]]; then
         REFSEQ="False"
+    else
+        # This script does not work for shotgun sequences, since they usually have 50+ partial sequences and 1 main one that is not annotated as a chromosome
+        ACC_LIST=(${RESULT//\s+/\s})
+        REFSEQ="True"
+        flag=false
+        for i in ${ACC_LIST[@]}
+        do
+            if $flag; then
+                CHROMOSOMES+=("$i")
+                echo "$i"
+                flag=false
+            elif [[ "$i" == "chromosome" ]]; then
+                flag=true
+            fi
+        done
     fi
-    echo -e ${BIOSAMPLE}'\t'${ACCESSION}'\t'${REFSEQ} >> ${MDATA}
+    echo -e ${BIOSAMPLE}'\t'${CHROMOSOMES[@]}'\t'${REFSEQ} >> ${MDATA}
 done
